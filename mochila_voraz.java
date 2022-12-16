d80b727a09fedd7928e1214eccc7cf645446e51d
santiago.higuita <shiguita2@alumno.uned.es>
2022-12-16 11:38:32 +0000

    first commit

---
+++ mochila_voraz.java
@@ -0,0 +1,28 @@
+/**
+ * mochila_voraz
+ */
+public class mochila_voraz {
+
+    public static void main(String[] args) {
+        switch (args.length) {
+            case 0:
+                
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+
+            default:
+                System.err.println(
+                    "Utilice -h para ver como ejecutar el programa"
+                );
+                break;
+        }
+    }
+}
